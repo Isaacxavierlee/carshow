8421b8b7140c064923418fba73d971c5745ea00b
Isaacxavierlee <reivax.caasi@gmail.com>
2024-07-06 19:39:53 +0800

    XIO

--- components/index.tsx
+++ components/index.tsx
@@ -2,10 +2,14 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
 import Hero from "./Hero";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";  
 
 export {
   Hero,
   CustomButton,
   NavBar,
   Footer,
+  SearchBar,
+  CustomFilter,
 }
